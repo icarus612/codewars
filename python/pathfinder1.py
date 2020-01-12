8138aa6909c781bd72a803d47e680ed760d42dde
icarus <ellishogan95@gmail.com>
2020-01-11 21:04:39 -0500

    update

--- python/pathfinder1.py
+++ python/pathfinder1.py
@@ -1,29 +1,28 @@
 # https://www.codewars.com/kata/path-finder-number-1-can-you-reach-the-exit/train/python
 
 def path_finder(maze):
-	m = maze.split("\n")
-	open_spaces = []
-	to_visit = [(0,0)]
-	end_block = (len(m)-1, len(m[0])-1)
-	for i, y in enumerate(m):
-		for j, x in enumerate(y):
-			if x != "W":
-				open_spaces.append((i, j))
-	for x in to_visit:
-		if x == end_block:
-			return True
-		if x in open_spaces:
-			for i in open_spaces:
-				if x[0] == i[0] and x[1]+1 == i[1]:
-					to_visit.append(i)
-				if x[0] == i[0] and x[1]-1 == i[1]:
-					to_visit.append(i)
-				if x[0]+1 == i[0] and x[1] == i[1]:
-					to_visit.append(i)
-				if x[0]-1 == i[0] and x[1] == i[1]:
-					to_visit.append(i)
-			open_spaces.remove(x)
-	return False
+    m = [list(i) for i in maze.split("\n")]
+    visited = []
+    to_visit = [(0,0)]
+    end_block = (len(m)-1, len(m[0])-1)
+    for x in to_visit:
+        if x == end_block:
+            return True
+        if x not in visited:
+            if x[0]+1 < len(m):
+                if m[x[0]+1][x[1]] != "W":
+                    to_visit.append((x[0]+1,x[1]))
+            if x[0]-1 > 0:
+                if m[x[0]-1][x[1]] != "W":
+                    to_visit.append((x[0]-1,x[1]))
+            if x[1]+1 < len(m[x[0]]):
+                if m[x[0]][x[1]+1] != "W":
+                    to_visit.append((x[0],x[1]+1))
+            if x[1]-1 > 0:
+                if m[x[0]][x[1]-1] != "W":
+                    to_visit.append((x[0],x[1]-1))
+            visited.append(x)
+    return False
 
 a = "\n".join([
 ".W.",
