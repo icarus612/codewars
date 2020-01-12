863deb59b56ad8acc4e2fa43023621c4a2141b02
icarus <ellishogan95@gmail.com>
2020-01-11 21:03:33 -0500

    update

--- python/pathfinder1.py
+++ python/pathfinder1.py
@@ -1,30 +1,28 @@
 # https://www.codewars.com/kata/path-finder-number-1-can-you-reach-the-exit/train/python
+
 def path_finder(maze):
 	m = maze.split("\n")
 	open_spaces = []
-	visited = []
 	to_visit = [(0,0)]
 	end_block = (len(m)-1, len(m[0])-1)
-	def look_around(spot):
-		for i in open_spaces:
-			if spot[0] == i[0] and spot[1]+1 == i[1]:
-				to_visit.append(i)
-			if spot[0] == i[0] and spot[1]-1 == i[1]:
-				to_visit.append(i)
-			if spot[0]+1 == i[0] and spot[1] == i[1]:
-				to_visit.append(i)
-			if spot[0]-1 == i[0] and spot[1] == i[1]:
-				to_visit.append(i)
 	for i, y in enumerate(m):
 		for j, x in enumerate(y):
 			if x != "W":
 				open_spaces.append((i, j))
-	for i in to_visit:
-		if i == end_block:
+	for x in to_visit:
+		if x == end_block:
 			return True
-		if i not in visited:
-			visited.append(i)
-			look_around(i)
+		if x in open_spaces:
+			for i in open_spaces:
+				if x[0] == i[0] and x[1]+1 == i[1]:
+					to_visit.append(i)
+				if x[0] == i[0] and x[1]-1 == i[1]:
+					to_visit.append(i)
+				if x[0]+1 == i[0] and x[1] == i[1]:
+					to_visit.append(i)
+				if x[0]-1 == i[0] and x[1] == i[1]:
+					to_visit.append(i)
+			open_spaces.remove(x)
 	return False
 
 a = "\n".join([
