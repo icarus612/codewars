02a8febb3a2a186820039d79c97eb4f4048f4e30
icarus <ellishogan95@gmail.com>
2020-01-20 22:09:37 -0500

    update

--- python/parityOutlier.py
+++ python/parityOutlier.py
@@ -1,14 +1,8 @@
 # https://www.codewars.com/kata/5526fc09a1bbd946250002dc
 
 def find_outlier(integers):
-    x = []
-    for i in integers[0:3]:
-        x.append(abs(i) % 2)
-    if x.count(1) > 1:
-        for i in integers:
-            if abs(i) % 2 == 0:
-                return i
-    else:
-         for i in integers:
-            if abs(i) % 2 == 1:
-                return i
+    x = [abs(i) % 2 for i in integers[0:3]]
+    n = 0 if x.count(1) > 1 else 1
+    for i in integers:
+        if abs(i) % 2 == n:
+            return i
